308b47aea23a0f7a4f6bc2cd49293a23a1578746
Markus Pöschl <Poeschl@users.noreply.github.com>
2021-01-06 11:23:43 +0100

    Add input_type parameter

--- tests/test_marytts.py
+++ tests/test_marytts.py
@@ -1,4 +1,3 @@
-import logging
 import unittest
 import wave
 from io import BytesIO
@@ -27,3 +26,24 @@ class TestMaryTTS(unittest.TestCase):
         self.assertEqual(waves.getnchannels(), 1)
         self.assertEqual(waves.getframerate(), 48000)
         self.assertGreater(waves.getnframes(), 10000)
+
+    def test_speak_as_emotionalxml(self):
+        mary = MaryTTS(input_type="EMOTIONML")
+
+        xml = """
+        <emotionml version="1.0" xmlns="http://www.w3.org/2009/10/emotionml" 
+        category-set="http://www.w3.org/TR/emotion-voc/xml#everyday-categories">        
+            <emotion><category name="angry"/>
+                Hello World
+            </emotion>
+        </emotionml>
+        """
+
+        audio = mary.speak(xml)
+        waves = wave.open(BytesIO(audio))
+
+        self.assertEqual(waves.getnchannels(), 1)
+        self.assertEqual(waves.getframerate(), 48000)
+        self.assertGreater(waves.getnframes(), 10000)
+        # Make sure only "Hello World" is in the output, not the whole xml
+        self.assertLess(len(audio), 200000)
